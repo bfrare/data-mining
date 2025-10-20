8979496b92abbbd21261b6f67109357c4f861745
bafra <inthenameofbafra@gmail.com>
2025-10-21 01:50:08 +0330

    read_text_file

--- # read_text_file.py
+++ # read_text_file.py
@@ -7,10 +7,11 @@ if os.path.exists(text_path):
     try:
         with open(text_path, "r", encoding="utf-8", errors="replace") as f:
             text = f.read()
-        print("✅ Text file length:", len(text))
+        print("✅ :", len(text))
         print("\nPreview :\n")
         print(text[:])
     except Exception as e:
         print(f"❌ : {e}")
 else:
+
     print(f"⚠️ : {text_path}") 
